670559c441549777a74ed14b40fdc70dbbd53b5f
unknown <cbereber7@gmail.com>
2024-02-21 21:54:55 +0800

    bruh

---
+++ script.js
@@ -0,0 +1,19 @@
+var acc = document.getElementsByClassName("accordion");
+
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    for (let j = 0; j < acc.length; j++) {
+    acc[j].classList.remove("active");
+      if(j!=i){
+        acc[j].nextElementSibling.style.maxHeight = null;
+      }
+    }
+    this.classList.add("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
